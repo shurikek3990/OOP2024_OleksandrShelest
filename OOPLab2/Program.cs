deca97b7495d3c2c3991dc8186d3423f447763fd
Oleksandr Shelest <w69949@student.wsiz.edu.pl>
2024-10-17 11:23:52 +0200

    first commit

---
+++ OOPLab2/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OOPLab2.Constructions;
+var construction1 = new Construction();
+var construction2 = new Construction(23.0f, 45.0f, 4, 4, "Material");
+Console.WriteLine("Hello World");
